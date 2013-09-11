9fcf311a360665020f9e4789e234c2d322c939a4
gpmitchell27 <gpmitchell27@fullsail.edu>
2013-09-11 03:28:58 -0400

    flickr api working correctly

--- searchandflick/CodeIgniter/application/controllers/gallery.php
+++ searchandflick/CodeIgniter/application/controllers/gallery.php
@@ -4,11 +4,12 @@ class Gallery extends CI_Controller {
 		parent::__construct();
 	}
 	public function index() {
-		$data['photos'] = $this;
+
+		$this->load->model('FlickrPhoto_model');
+		$photos['photos'] = $this->FlickrPhoto_model->getPhotos();
+		$data['photos'] = $photos;
 		$this->load->view('searchandflick/header.inc');
 		$this->load->view('searchandflick/body.inc', $data);
 		$this->load->view('searchandflick/footer.inc');
-		$this->load->model('FlickrPhoto_model');
-		$result['get_photo'] = $this->FlickrPhoto_model->getPhotos();
 	}
 }
